ce72a4641182c9dd4b7194461b5e0e459eac0bda
SID37 <1999login@mail.ru>
2023-05-13 13:30:35 +0300

    Package configuration

---
+++ Runtime/AirFluid.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AirFluid
+{
+    public class AirFluid : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
